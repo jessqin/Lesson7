087cdbdf1c00e4848bc4ace07e5ac1ef07355ba5
Jessica N Qin <jq210@pvhs-lab112-d15.pvsd.org>
2016-09-16 14:31:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//not on the AP exam
+        Scanner in = new Scanner(System.in);
+        //step 1 create the scanner
+        //step 2 ask you user
+        System.out.println("enter favorite int");
+        //step 3 record input
+        int i = in.nextInt();
+        System.out.println("enter favorite double");
+        Scanner intString = new
+        double d = in.nextDouble();
+        System.out.println("Enter Favorite Color");
+        String color = in.nextLine();
+    }
+}
